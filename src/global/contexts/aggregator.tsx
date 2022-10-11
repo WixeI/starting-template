7fa89d794afaaed00f609a581e5c009b25126fb5
Paulo <pauloportelamartins@gmail.com>
2022-10-11 01:24:32 -0300

    Added more Stitches configurations

--- src/global/contexts/aggregator.tsx
+++ src/global/contexts/aggregator.tsx
@@ -1,8 +1,6 @@
 import { I18nextProvider } from 'react-i18next';
 import languageInstance from '../../configs/i18n/config';
 import { ReactNode } from 'react';
-import '../styles/defaultStyles/index.css';
-import '../styles/resetStyles/index.css';
 
 interface ContextAggregatorProps {
   children: ReactNode;
